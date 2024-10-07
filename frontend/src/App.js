8d5794dea44b0d15973dbae4066bf82f010171e2
Pavel Darmovzal <paveldarmovzal1@gmail.com>
2024-10-07 12:56:35 +0200

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,68 +9,97 @@ function App() {
   const baseUrl = process.env.REACT_APP_API_URL ? process.env.REACT_APP_API_URL : apiUrl
 
 
-
-  const addNote = (note) => {
-    setNotes(prevNotes => {
-      return [...prevNotes, note]
-    })
+// GET ALL NOTES
+  const fetchData = async () => {
     const url = `${baseUrl}/notes/`
-    fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        title: note.title,
-        content: note.content,
-      })
-    })
-    console.log(`Add note: ${url}`)
-    console.log(notes)
-  }
 
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setNotes(data);
+    } catch (error) {
+      console.log(error)
+    }
 
-  const handleDelete = async (note) => {
-    const url = `${baseUrl}/notes/delete/${note.id}/`
-    await fetch(url, { method: 'DELETE' });
-    setNotes(prevNotes => {
-      return prevNotes.filter(n => {
-        return n.id !== note.id
-      })
-    })
-    console.log(`Delete note: ${url}`)
-  }
-
-
-  const updateNote = (id, newTitle) => {
-    const url = `${baseUrl}/notes/update/${id}/`
-    fetch(url, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        title: newTitle
-      })
-    })
-    console.log(`Update note: ${url}`)
-
-  }
-
-
-
-  const fetchData = useCallback(async () => {
-    const url = `${baseUrl}/notes/`
-    const response = await fetch(url);
-    const data = await response.json();
-    setNotes(data);
-    console.log(`Get notes: ${url}`)
-  }, [baseUrl]);
-
+  };
 
   useEffect(() => {
     fetchData();
-  }, [fetchData]);
+  }, []);
+
+
+
+// ADD A NOTE
+  const addNote = async (note) => {
+    const newNote = {
+      title: note.title,
+      content: note.content
+    }
+
+    try {
+      const url = `${baseUrl}/notes/`
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newNote)
+      })
+      const data = await response.json()
+      setNotes(prevNotes => {
+        return [...prevNotes, data]
+      })
+
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
+
+// DELETE A NOTE
+  const deleteNote = async (id) => {
+    const url = `${baseUrl}/notes/delete/${id}/`
+
+    try {
+      await fetch(url, { method: 'DELETE' });
+      setNotes(prevNotes => prevNotes.filter((n) => n.id !== id))
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+
+
+// UPDATE A NOTE
+  const updateNote = async (id, newTitle) => {
+    const url = `${baseUrl}/notes/update/${id}/`
+
+    try {
+      const response = await fetch(url, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          title: newTitle
+        })
+      })
+      const data = await response.json()
+      setNotes((prevNotes) =>
+        prevNotes.map((note) => {
+          if (note.id === id) {
+            return data;
+          } else {
+            return note;
+          }
+        })
+      );
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
 
   return (
@@ -79,7 +108,7 @@ function App() {
         <div key={note.id} className='wrapper'>
           <h3>{note.title}</h3>
           <p>{note.content}</p>
-          <button onClick={() => handleDelete(note)}>Delete</button>
+          <button onClick={() => deleteNote(note.id)}>Delete</button>
           <Update updateNote={updateNote} id={note.id} />
         </div>
       ))}
